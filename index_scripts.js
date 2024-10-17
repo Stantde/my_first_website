255cefcf8912d7e1482a6de0277e66b84dfb2342
Stantde <125204126+Stantde@users.noreply.github.com>
2024-10-17 16:49:27 -0500

    These are the files which were finally submitted in fulfillment of cs50's homepage assignment.
    	modified:   bar.html
    	modified:   baz.html
    	modified:   index.html
    	modified:   index_scripts.js
    	modified:   specification.txt
    	modified:   styles.css

--- index_scripts.js
+++ index_scripts.js
@@ -4,6 +4,26 @@ console.log(randomNumber);
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+function checkGuess(userGuess){
+  let number = document.getElementById("user_guess1").value;
+  if(number == ''){
+     document.querySelector(".errorResponse").innerHTML = "There's nothing here, you donut!";
+     document.querySelector(".validResponse").innerHTML = "";
+     console.log("There's nothing here, you donut!");
+  } else if (number > randomNumber){
+    document.querySelector(".errorResponse").innerHTML = "";
+    document.querySelector(".validResponse").innerHTML = "That number: " + number + " is too high!";
+    console.log("too high");
+  } else if (number < randomNumber){
+      document.querySelector(".errorResponse").innerHTML = "";
+      document.querySelector(".validResponse").innerHTML = "That number: " + number + " is too low!";
+      console.log("too low");
+  } else if ( number == randomNumber){
+      document.querySelector(".errorResponse").innerHTML = "";
+      document.querySelector(".validResponse").innerHTML = "That's correct!";
+      console.log("got it");
+  }
+}
 
 if ( ! (randomNumber)){
 	randomNumber = getRandomInt(100) + 1;
